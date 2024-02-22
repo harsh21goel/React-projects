4cce8812555a88d5c9fb63ce900a60a5e38dcfc0
Harsh goel <harshgoel561@gmail.com>
2024-02-22 15:50:52 +0530

    add new project

--- reactRouter/src/components/Header/Header.jsx
+++ reactRouter/src/components/Header/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
               to="#"
               className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
-              Get started
+              Get started 
             </Link>
           </div>
           <div
