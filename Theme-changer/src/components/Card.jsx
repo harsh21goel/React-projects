4e76c5714119ed68dfb80d20393faa884ebe4fb5
Harsh goel <harshgoel561@gmail.com>
2024-03-01 12:44:02 +0530

    change image of card

--- Theme-changer/src/components/Card.jsx
+++ Theme-changer/src/components/Card.jsx
@@ -2,12 +2,12 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://i.pinimg.com/564x/c1/86/c8/c186c89ed17db6677ae7d2c1f9592b25.jpg" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        Savage Billa, Aluminium Case, Starlight Sport
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
@@ -61,7 +61,7 @@ export default function Card() {
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$100M</span>
                     <a
                         href="/"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
